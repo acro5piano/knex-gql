b94d5549b2b9b27b71ec2fb3bb5f998ce0867a29
gosho-kazuya <ketsume0211@gmail.com>
2021-07-23 14:08:45 +0900

    filter `returning`

--- src/directives/InsertDirective.ts
+++ src/directives/InsertDirective.ts
@@ -1,3 +1,4 @@
+import { filterGraphQLSelections } from '../execution/filterSelection'
 import { createFieldManipulator } from '../schema/directive/createFieldManipulator'
 import { gql, resolveFirst } from '../util'
 
@@ -7,9 +8,18 @@ export const InsertDirective = createFieldManipulator({
     directive @insert on FIELD_DEFINITION
   `,
   schemaMapper: ({ fieldConfig, targetTableName, knexGql }) => {
-    fieldConfig.resolve = (_root, args) => {
+    fieldConfig.resolve = (_root, args, _ctx, info) => {
       return resolveFirst(
-        knexGql.knex(targetTableName).insert(args['input']).returning('*'),
+        knexGql
+          .knex(targetTableName)
+          .insert(args['input'])
+          .returning(
+            filterGraphQLSelections({
+              info,
+              knexGql,
+              table: targetTableName!,
+            }),
+          ),
       ) // TODO: filter keys based on node field set selection
     }
     return fieldConfig
